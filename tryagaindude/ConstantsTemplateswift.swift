6ddc47dc576c4de69c993c03a8f4407ad5222358
Nikita <n.nerozin@gmail.com>
2021-01-03 20:39:49 +0000

    modified git ignore to sotp

---
+++ tryagaindude/ConstantsTemplateswift.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  tryagaindude
+//
+//  Created by user180923 on 1/2/21.
+//
+
+import Foundation
+
+  /*  struct Constants {
+
+static var API_KEY = ""
+static var PLAYLIST_ID = ""
+static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+}*/
+
