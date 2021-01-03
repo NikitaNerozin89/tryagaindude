d7716e023624913cfb569769448951e6177d6b5b
Nikita <n.nerozin@gmail.com>
2021-01-03 20:27:25 +0000

    modified gitignore

---
+++ tryagaindude/Video.swift
@@ -0,0 +1,18 @@
+//
+//  Video.swift
+//  tryagaindude
+//
+//  Created by user180923 on 1/2/21.
+//
+
+import Foundation
+
+struct Video {
+    
+    var videoId = ""
+    var title = ""
+    var description = ""
+    var thumbnail = ""
+    var published = ""
+    
+}
